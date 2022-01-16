656e3616e0331daab72d3fe896ef1a93a3aa2908
Alexander Zaslonov <zaslonov.alexander@gmail.com>
2022-01-15 18:00:05 -0800

    Added designer wrapper component.

---
+++ src/components/paperbitsDesigner.tsx
@@ -0,0 +1,20 @@
+import * as React from "react";
+import { attachDesignerTo } from "@paperbits/common/ko/utils";
+
+export class PaperbitsDesigner extends React.Component {
+    private readonly ref = React.createRef<HTMLDivElement>();
+
+    constructor(props) {
+        super(props);
+    }
+
+    public async componentDidMount(): Promise<void> {
+        attachDesignerTo(this.ref.current);
+    }
+
+    public render(): JSX.Element {
+        return (
+            <div className="paperbits-designer" ref={this.ref}>Hello</div>
+        );
+    }
+}
